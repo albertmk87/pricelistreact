620bf15bce4c8c7294994395f3ae3b75b1de7f7b
Albert Miftar <a.miftar@aa.mk>
2020-07-09 13:25:33 +0200

    Updates

---
+++ precache-manifest.280d2fdf59d3f3d1adf38471fe8ee657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0dd4b50ef8a96c955e7157bcf42ebc",
+    "url": "/pricelistreact/index.html"
+  },
+  {
+    "revision": "5d1af7ab365f29ec06f2",
+    "url": "/pricelistreact/static/css/main.4fd3ab26.chunk.css"
+  },
+  {
+    "revision": "016316156902f899469f",
+    "url": "/pricelistreact/static/js/2.9d768199.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pricelistreact/static/js/2.9d768199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1af7ab365f29ec06f2",
+    "url": "/pricelistreact/static/js/main.ed4c44b5.chunk.js"
+  },
+  {
+    "revision": "dbb9baaf242c10a37557",
+    "url": "/pricelistreact/static/js/runtime-main.8472903c.js"
+  }
+]);
